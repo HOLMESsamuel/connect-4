531db4a483e5ca278c0c34bbfbae64a3e7c12bdf
Samuel Holmes <47476255+HOLMESsamuel@users.noreply.github.com>
2019-06-30 09:04:20 +0200

    Add files via upload

---
+++ connect 4/js/main.js
@@ -0,0 +1,226 @@
+class grid {
+    constructor(selector) {
+        this.COLS = 7;
+        this.ROWS = 6;
+        this.selector = selector;
+        this.createGrid();
+        this.gameNumber = 0;
+    }
+
+    createGrid() {
+        const $board = $(this.selector);
+        for (let row = 0; row < this.ROWS; row++) {
+            const $row = $('<div>').addClass('row');
+            for (let col = 0; col < this.COLS; col++) {
+                const $col = $('<div>').addClass('column empty').attr("data-row", row).attr("data-col", col);
+                $row.append($col);
+            }
+            $board.append($row);
+        }
+    }
+
+    findLastEmptyCell(col) {
+        //search all the cells on the column col and return an array of them
+        const cells = $(`.column[data-col='${col}']`);
+        for (let i = cells.length - 1; i >= 0; i--) {
+            const $cell = $(cells[i]);
+            if ($cell.hasClass("empty")) {
+                return $cell;
+            }
+        }
+        return null;
+    }
+
+    getCell(row, col) {
+        const cell = $(`.column[data-col='${col}'][data-row='${row}']`);
+        return cell;
+    }
+
+
+    /**the fonction checks for an alignement of 4 colors in one of four directions
+     * (South-North, East-West, SouthWest-NorthEast, NorthWest-SouthEsast)
+     * It work thanks to a recursive method 
+     */
+    checkForWinner(row, col, player) {
+        const that = this; //that will be used as this in the following functions
+
+        function checkS(n, row, col, player) {
+            if (row < 5) { //verify we are not looking outside the grid
+                if (that.getCell(row + 1, col).hasClass(player)) {
+                    return checkS(n + 1, row + 1, col, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        function checkE(n, row, col, player) {
+            if (col < 6) {
+                if (that.getCell(row, col + 1).hasClass(player)) {
+                    return checkE(n + 1, row, col + 1, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        function checkW(n, row, col, player) {
+            if (col > 0) {
+                if (that.getCell(row, col - 1).hasClass(player)) {
+                    return checkW(n + 1, row, col - 1, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        function checkNE(n, row, col, player) {
+            if (col < 6 && row > 0) {
+                if (that.getCell(row - 1, col + 1).hasClass(player)) {
+                    return checkNE(n + 1, row - 1, col + 1, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        function checkSW(n, row, col, player) {
+            if (col > 0 && row < 5) {
+                if (that.getCell(row + 1, col - 1).hasClass(player)) {
+                    return checkSW(n + 1, row + 1, col - 1, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        function checkNW(n, row, col, player) {
+            if (col > 0 && row > 0) {
+                if (that.getCell(row - 1, col - 1).hasClass(player)) {
+                    return checkNW(n + 1, row - 1, col - 1, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        function checkSE(n, row, col, player) {
+            if (col < 6 && row < 5) {
+                if (that.getCell(row + 1, col + 1).hasClass(player)) {
+                    return checkSE(n + 1, row + 1, col + 1, player);
+                } else {
+                    return n;
+                }
+            } else {
+                return n;
+            }
+        }
+
+        if (checkS(1, row, col, player) >= 4) {
+            return player + " wins !";
+        } else if (checkE(1, row, col, player) + checkW(0, row, col, player) >= 4) {
+            return player + " wins !";
+        } else if (checkNE(1, row, col, player) + checkSW(0, row, col, player) >= 4) {
+            return player + " wins !";
+        } else if (checkNW(1, row, col, player) + checkSE(0, row, col, player) >= 4) {
+            return player + " wins !";
+        } else {
+            return null;
+        }
+    }
+
+    AIturn(player) {
+        const randomCol = Math.floor(Math.random() * 6);
+        const cell = this.findLastEmptyCell(randomCol);
+        cell.removeClass("empty");
+        const ai = (player === 'red') ? "yellow" : "red";
+        cell.addClass(ai);
+        const row = cell.data("row");
+        if (this.checkForWinner(row, randomCol, ai)) {
+            alert(this.checkForWinner(row, randomcol, ai));
+        }
+    }
+
+    /**Sets the game mode and the player 1 color
+     * It makes the AI begin randomly one over two times
+     */
+    setMode(mode, color) {
+        this.mode = mode;
+        this.player = color;
+        if (Math.random() >= 0.5 && mode == "AI") {
+            this.AIturn(color);
+        }
+    }
+
+
+    createEventListeners() {
+        const $board = $(this.selector);
+        const that = this; //that will be used as this in the following functions
+
+
+        $board.on("mouseenter", ".column.empty", function() {
+            const col = $(this).data("col");
+            const lastEmptyCell = that.findLastEmptyCell(col);
+            lastEmptyCell.addClass("next-" + that.player);
+        });
+
+        $board.on("mouseleave", ".column", function() {
+            $(".column").removeClass("next-" + that.player);
+        });
+
+        $board.on("click", ".column.empty", function() {
+            const col = $(this).data("col");
+            const lastEmptyCell = that.findLastEmptyCell(col);
+            lastEmptyCell.removeClass("empty");
+            lastEmptyCell.addClass(that.player);
+            const row = lastEmptyCell.data("row");
+            if (that.checkForWinner(row, col, that.player)) {
+                $board.off("click");
+                $board.off("mouseenter");
+                $board.off("mouseleave");
+                that.gameNumber = 0;
+                alert(that.checkForWinner(row, col, that.player));
+            }
+            //if the player is red we change it to yellow
+            if (that.mode == "2players") {
+                that.player = (that.player === 'red') ? "yellow" : "red";
+                $(this).trigger("mouseenter");
+            } else {
+                that.AIturn(that.player);
+            }
+
+        });
+    }
+}
+
+$(document).ready(function() {
+    $grid = new grid("#connect4");
+    $("#begin").click(function() {
+        $("#connect4").empty();
+        $grid.createGrid();
+        color = $('input[name=inlineRadioOptions2]:checked').val();
+        if ($('input[name=inlineRadioOptions1]:checked').val() == "AI") {
+            $grid.setMode("AI", color);
+        } else {
+            $grid.setMode("2players", color);
+        }
+        if ($grid.gameNumber == 0) {
+            $grid.createEventListeners();
+            $grid.gameNumber += 1;
+        }
+
+
+    });
+});
